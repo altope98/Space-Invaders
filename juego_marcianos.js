56f75596dfcc25efd0be9cec723558196fea81e0
Álvaro Torrente Pérez <alvarotorrenteperez98@gmail.com>
2019-11-28 19:36:40 +0100

    Disparo hecho 28/11/2019

--- juego_marcianos.js
+++ juego_marcianos.js
@@ -2,8 +2,6 @@ export class Marciano{
     constructor(x,y){
         this.x=x;
         this.y=y;
-    }
-    dibujar(){
         this.mar=document.createElementNS("http://www.w3.org/2000/svg","rect");
             this.mar.setAttribute("width","40");
             this.mar.setAttribute("fill", "green");
@@ -11,21 +9,16 @@ export class Marciano{
             this.mar.setAttribute("x", this.x);
             this.mar.setAttribute("y", this.y);
     }
+    dibujar(){
+        this.mar.setAttribute("x", this.x);
+    }
     mover(direccion) {
-        //console.log(this.largo);
-        if(direccion=="-"){
-            this.x = this.x - 10;
-        }else{
-            this.x = this.x + 10;
-        }
-        if (this.x <= 0){
-            this.x=5;
-        }
-        if(this.x  >= 500) {
-            this.x=450-5;
-        }
-
-        this.dibujar();
+            if(direccion=="-"){
+                this.x = this.x - 10;
+            }else{
+                this.x = this.x + 10;
+            }
+            this.dibujar();
     }
 }
 
@@ -33,10 +26,11 @@ export class Nave{
     constructor(){
         this.x= 500/2;
         this.velx=20;
+        this.y=400;
         this.tag = document.createElementNS("http://www.w3.org/2000/svg", "rect");
         this.tag.setAttribute("fill", "blue");
         this.tag.setAttribute("x", this.x);
-        this.tag.setAttribute("y", 400);
+        this.tag.setAttribute("y", this.y);
         this.tag.setAttribute("width", 50);
         this.tag.setAttribute("height", 20);
     }
@@ -57,7 +51,6 @@ export class Nave{
         this.dibujar();
     }
 
-    
     dibujar() {
         this.tag.setAttribute("x", this.x);
     }
@@ -65,6 +58,43 @@ export class Nave{
 }
 
 export class Disparo{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.shot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        this.shot.setAttribute("fill", "red");
+        this.shot.setAttribute("r", "10");
+        this.shot.setAttribute("cx", this.x);
+        this.shot.setAttribute("cy", this.y);
+
+        this.svg=document.getElementById("svg1");
+        this.div=document.getElementById("juego");
+
+        this.svg.appendChild(this.shot);
+        this.div.appendChild(this.svg);
+
+        this.mover();
+
+    }
+
+    mover() {
+        
+        do{
+        setInterval(()=>{
+            
+                this.shot.setAttribute("cy", this.y-=10);
+                
+        },100);
+    }while(0>=this.y)
+            
+        
+            
+        
+        
+    }
+
+
+
 
 }
 
@@ -90,8 +120,9 @@ export class Juego{
         let svg=document.createElementNS("http://www.w3.org/2000/svg", "svg");
             svg.setAttribute("width","500");
             svg.setAttribute("height","500");
+            svg.setAttribute("id","svg1");
         for (this.marciano of this.marcianos){   ///CAMBIAR EL PINTADO PARA QUE CUANDO DESAPAREZCA DEL ARRAY NO PINTE NADA EN ESA POSICION CON UN IF
-            this.marciano.dibujar();
+            //this.marciano.dibujar();
             this.marciano.mar.setAttribute("id", cont);
             cont++;
             svg.appendChild(this.marciano.mar);
--- spaceinvaders.js
+++ spaceinvaders.js
@@ -1,14 +1,14 @@
 import {Marciano, Nave, Disparo, Juego} from './juego_marcianos.js';
 
-function moverMarciano(){
-
-}
 
 
 function movimiento(evento) {
 
     if(evento.keyCode==32){
-        //disparar();
+
+        let x=juego.nave.x+(50/2);
+        let y=juego.nave.y;
+        let disparo= new Disparo(x,y);
     }
     if(evento.keyCode==37){
         juego.nave.mover("-");
@@ -18,23 +18,33 @@ function movimiento(evento) {
     }
 }
 var juego;
+var izquierda;
+var derecha=false;
 window.onload=() => {
-    //document.body.addEventListener("keyup",movimiento);
     juego =new Juego();
     juego.dibujar();
     document.body.addEventListener("keyup",movimiento);
     setInterval(()=>{
-        for ( this.marciano of juego.marcianos) {
-            if(this.marciano.x<= 450){
+        for ( let marciano of juego.marcianos) {
+           /*  if(this.marciano.x<= 450){
                 this.marciano.x+=10;
             }
             if(this.marciano.x?= 0){
                 this.marciano.x+=10;
+            } */
+
+
+            /*if (this.marciano.x <= 0){
+                this.marciano.mover("+");
+            }*/
+            if(marciano.x  > 490) {
+                derecha=true;
+                if(derecha)
+                marciano.mover("-");
+            }else{
+                derecha=false;
+                marciano.mover("+");
             }
-
-            
-
-            
         }
     },1000);
 }
